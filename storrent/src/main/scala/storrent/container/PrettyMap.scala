20164a8b09bed5621794a0ab8388bbbb6de11b06
SimiPro <sihuber@gmx.net>
2014-05-30 12:59:23 +0200

    refactoring

---
+++ storrent/src/main/scala/storrent/container/PrettyMap.scala
@@ -0,0 +1,19 @@
+package storrent.container
+
+import scala.collection.mutable.MutableList
+import scala.collection.mutable.LinkedHashMap
+
+class PrettyMap extends LinkedHashMap[String,Any] {
+
+  override def toString:String = {
+    var result = new StringBuilder()
+    if (!this.isEmpty) {
+      result.append("{")
+      result.append(this.head._1).append(" => ").append(this.head._2)
+      this.tail.foreach(T => result.append(',').append(T._1).append(" => ").append(T._2))
+      result.append("}")
+    }
+    result.toString
+  }
+  
+}
